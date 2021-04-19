a7ad9c918a55ffc71687a5b41dd8439cc548c00d
Qadree Trimble <qadree_t@yahoo.com>
2021-04-19 15:50:37 -0700

    finished lab7 with help from Daniel and Aloysious

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 'use strict';
-
+const cors = require('cors');
+//BACK END SERVER
 // const { response } = require('express'); 
+
 const express = require('express'); //This line of code is how we import express into the server.
 require('dotenv').config();
+const weatherData = require('./data/weather.json');
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(cors());
+console.log(weatherData);
 
 app.get('/', (request, response) => {
-    response.send('You are doing it Qadree!');
+    response.send('Hello, You got it!');
 });
-app.listen(PORT, () => console.log(`seerver is listening on port ${PORT}`));
+
+app.get('/weather', (request, response) => {
+      try {
+        const dailyForecasts  = weatherData.data.map(day => new Forecast(day));
+        response.send(dailyForecasts);
+    } catch (error) {
+        handleErrors(error, response);
+    }
+});
+
+function Forecast(obj) {
+    this.date = obj.datetime;
+    this.description = obj.weather.description;
+}
+
+function handleErrors (error, response) {
+    response.status(500).send('Internal Error');
+}
+
+// app.get('/', (request, response) => {
+//     response.json(lat)
+// })
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
 // const cors = require('cors';
 
 // const superagent = require('superagent');
